2e02f4d513f582da4ef512268045c9a93814936b
viet34tqc <viet34tqc@gmail.com>
2021-09-11 22:40:20 +0700

    save access_token

--- src/features/auth/authSaga.ts
+++ src/features/auth/authSaga.ts
@@ -1,30 +1,59 @@
-import { fork, take } from '@redux-saga/core/effects';
+import { call, delay, fork, put, take } from '@redux-saga/core/effects';
 import { PayloadAction } from '@reduxjs/toolkit';
-import { login, LoginPayload, logout } from './authSlice';
+import {
+	login,
+	loginFailed,
+	LoginPayload,
+	loginSuccess,
+	logout,
+} from './authSlice';
 
-function handleLogin(payload: LoginPayload) {
-	console.log('Handle login', payload);
+function* handleLogin(payload: LoginPayload) {
+	try {
+		yield delay(1000); // This should be API call
+		localStorage.setItem('access_token', 'fake token');
+
+		// This should add the real user retrieved from API call.
+		yield put(
+			loginSuccess({
+				id: 1,
+				name: 'viet',
+			})
+		);
+	} catch (error) {
+		yield put(loginFailed(error.message));
+	}
+	// Redirect to admin page
 }
 
 function handleLogout() {
 	console.log('Logged out');
+	localStorage.removeItem('access_token');
+
+	// Redirect to login page
 }
 
 function* watchLoginFlow() {
 	while (true) {
-		// wait for dispatching action login
-		// take is non-blocking call, it returns the action object that is being dispatched.
-		const action: PayloadAction<LoginPayload> = yield take(login.type);
-		// then handle login
-		yield fork(handleLogin, action.payload);
+		const isLoggedIn = Boolean(localStorage.getItem('access_token'));
+
+		if (!isLoggedIn) {
+			// wait for login action
+			// take is non-blocking call, it returns the action object that is being dispatched.
+			const action: PayloadAction<LoginPayload> = yield take(login.type);
+			// then handle login
+			yield fork(handleLogin, action.payload);
+		}
 
 		// Now the login is done
 		// If you click the login again, it won't execute the 'take' above again.
 
-		// wait for dispatching action logout
+		// wait for logout action
 		yield take(logout.type);
 		// then handle logout
-		yield fork(handleLogout);
+		// We are not using 'fork' here, because fork is non-blocking then it will comeback immediately to the top of the loop
+		// We need to wait for the logout process to be done completely
+		yield call(handleLogout);
 
 		// Now the logout is done
 		// Because we are in the loop, so return to the login handle again
--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, makeStyles, Paper, Typography } from '@material-ui/core';
 import { useAppDispatch } from '../../../app/hooks';
-import { login } from '../authSlice';
+import { login, logout } from '../authSlice';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -42,6 +42,14 @@ const LoginPage = () => {
 						variant="contained"
 						color="primary"
 						onClick={handleLoginClick}
+					>
+						Fake Login
+					</Button>
+					<Button
+						fullWidth
+						variant="contained"
+						color="primary"
+						onClick={() => dispatch(logout())}
 					>
 						Fake Login
 					</Button>
